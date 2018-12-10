bdc071f4fa8e3ca6a1e0f7cabc675607c772fdce
Glitch (cfa-rapidpro-bongolive) <none>
2018-12-10 18:29:28 +0000

    👨🐗 Updated with Glitch

--- server.js
+++ server.js
@@ -5,8 +5,10 @@
 const express = require('express');
 const app = express();
 
-const axios = require('axios');
 const bodyParser = require('body-parser');
+const querystring = require('querystring');   
+const https = require('https');
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(req, resp) {
@@ -17,16 +19,38 @@ app.get('/', function(req, resp) {
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/moyo-moshi', function (req, resp) {
-  axios.post(process.env.RP_URL_MOYO, {
-    from: req.body.SOURCEADDR,
-    text: req.body.MESSAGE
-  })
-  .then(function (response) {
-    return resp.send(response)
-  })
-  .catch(function (error) {
-    return resp.send(error)
+  var postData = querystring.stringify({
+    'from': req.body.SOURCEADDR,
+    'text': req.body.MESSAGE
   });
+
+  var options = {
+    hostname: process.env.RP_HOSTNAME,
+    port: 443,
+    path: process.env.RP_PATH_MOYO,
+    method: 'POST',
+    headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+         'Content-Length': postData.length
+       }
+  };
+  var req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+      process.stdout.write(d);
+      resp.send(d);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+    resp.send(e);
+  });
+
+  req.write(postData);
+  req.end();
 })
 
 // listen for requests :)
